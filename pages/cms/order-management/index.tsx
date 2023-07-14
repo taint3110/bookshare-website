bcf0fae777ba83892c55502d7dcccea9c4bf0e56
Thành Tài <taint0812@gmail.com>
2023-07-14 21:05:30 +0700

    BS-25 | Implement Login api

--- pages/cms/order-management/index.tsx
+++ pages/cms/order-management/index.tsx
@@ -1,7 +1,23 @@
+import { ETokenKey, PLATFORM } from 'API/constants'
 import CMSLayout from 'components/Layout/CMSLayout'
 import OrderManagement from 'components/pages/CMS/OrderManagement'
+import router from 'next/router'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import routes from 'routes'
+import { getAuthenticateStorageKey } from 'utils/common'
 
 const OrderManagementPage = () => {
+  useEffect(() => {
+    const token: ETokenKey = getAuthenticateStorageKey(PLATFORM.CMS)
+    const userToken: string = localStorage.getItem(token) ?? sessionStorage.getItem(token) ?? ''
+    if (!userToken) {
+      toast.error('Please login to continue', {
+        toastId: 'Login'
+      })
+      router.replace(routes.cms.login.value)
+    }
+  }, [])
   return (
     <CMSLayout title={`Order Management | Internal Portal`} topBarTitle="Order Management">
       <OrderManagement />
