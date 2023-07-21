6a9c64e652e6c5f1a89815340c6350c42730d386
Thành Tài <taint0812@gmail.com>
2023-07-21 22:33:12 +0700

    hotfix

--- components/pages/Cart/index.tsx
+++ components/pages/Cart/index.tsx
@@ -47,69 +47,78 @@ const Cart = () => {
     }
   }, [user])
 
-  if (
-    getValidArray(getValidArray(websiteOrderList?.results)[0]?.bookList).filter(
-      (book: IBookWithRelations) => book.bookStatus === EBookStatusEnum.ORDERED
-    ).length === 0
-  ) {
+  function checkIsEmptyCart(): boolean {
     return (
-      <Stack pl="280px" pr="280px" marginTop="12" mb="40" align={'center'}>
-        <Image
-          alt="empty cart"
-          width={600}
-          src="https://assets.materialup.com/uploads/16e7d0ed-140b-4f86-9b7e-d9d1c04edb2b/preview.png"
-        ></Image>
-        <Text fontSize={'2xl'}>Your cart is empty.</Text>
-      </Stack>
+      getValidArray(getValidArray(websiteOrderList?.results)[0]?.bookList).filter(
+        (book: IBookWithRelations) => book.bookStatus === EBookStatusEnum.ORDERED
+      ).length === 0
     )
   }
-  if (isCollapsed) {
-    return getValidArray(websiteOrderList?.results)
-      .filter(
-        (order: IOrder) =>
-          getValidArray(order.bookList).filter(
-            (book: IBookWithRelations) => book.bookStatus === EBookStatusEnum.ORDERED
-          ).length > 0
-      )
-      .map((order: IOrder) => (
-        <Stack
-          key={order.id}
-          paddingLeft="20px"
-          paddingRight="20px"
-          marginTop="8"
-          marginBottom="12"
-          spacing={4}
-          alignItems={'flex-start'}
-        >
-          <Container width={'100%'}>
-            <CartBookList order={order} />
-          </Container>
-          <Container width={'100%'}>
-            <CartUserInfo order={order} />
-          </Container>
-          <Divider />
+
+  return (
+    <>
+      {checkIsEmptyCart() && (
+        <Stack pl="280px" pr="280px" marginTop="12" mb="40" align={'center'}>
+          <Image
+            alt="empty cart"
+            width={600}
+            src="https://assets.materialup.com/uploads/16e7d0ed-140b-4f86-9b7e-d9d1c04edb2b/preview.png"
+          ></Image>
+          <Text fontSize={'2xl'}>Your cart is empty.</Text>
         </Stack>
-      ))
-  }
-  return getValidArray(websiteOrderList?.results)
-    .filter(
-      (order: IOrder) =>
-        getValidArray(order.bookList).filter((book: IBookWithRelations) => book.bookStatus === EBookStatusEnum.ORDERED)
-          .length > 0
-    )
-    .map((order: IOrder) => (
-      <>
-        <HStack key={order?.id} marginTop="8" marginBottom="12" alignItems="flex-start" justify="center">
-          <Container marginRight={0}>
-            <CartBookList order={order} />
-          </Container>
-          <Container marginLeft={0}>
-            <CartUserInfo order={order} />
-          </Container>
-        </HStack>
-        <Divider />
-      </>
-    ))
+      )}
+      {isCollapsed &&
+        !checkIsEmptyCart() &&
+        getValidArray(websiteOrderList?.results)
+          .filter(
+            (order: IOrder) =>
+              getValidArray(order.bookList).filter(
+                (book: IBookWithRelations) => book.bookStatus === EBookStatusEnum.ORDERED
+              ).length > 0
+          )
+          .map((order: IOrder) => (
+            <Stack
+              key={order.id}
+              paddingLeft="20px"
+              paddingRight="20px"
+              marginTop="8"
+              marginBottom="12"
+              spacing={4}
+              alignItems={'flex-start'}
+            >
+              <Container width={'100%'}>
+                <CartBookList order={order} />
+              </Container>
+              <Container width={'100%'}>
+                <CartUserInfo order={order} />
+              </Container>
+              <Divider />
+            </Stack>
+          ))}
+      {!isCollapsed &&
+        !checkIsEmptyCart() &&
+        getValidArray(websiteOrderList?.results)
+          .filter(
+            (order: IOrder) =>
+              getValidArray(order.bookList).filter(
+                (book: IBookWithRelations) => book.bookStatus === EBookStatusEnum.ORDERED
+              ).length > 0
+          )
+          .map((order: IOrder) => (
+            <>
+              <HStack key={order?.id} marginTop="8" marginBottom="12" alignItems="flex-start" justify="center">
+                <Container marginRight={0}>
+                  <CartBookList order={order} />
+                </Container>
+                <Container marginLeft={0}>
+                  <CartUserInfo order={order} />
+                </Container>
+              </HStack>
+              <Divider />
+            </>
+          ))}
+    </>
+  )
 }
 
 export default observer(Cart)
