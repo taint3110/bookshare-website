6508c111c5e2a8837c8b9aac3e3b42cd8eb31f6f
Thành Tài <taint0812@gmail.com>
2023-07-21 20:29:22 +0700

    Update API_URL

--- API/index.ts
+++ API/index.ts
@@ -7,7 +7,7 @@ import { getAuthenticateStorageKey } from 'utils/common'
 import { ETokenKey, IServerError, PLATFORM, ServerErrorMessage } from './constants'
 import get from 'lodash/get'
 
-const API_URL = 'http://localhost:3001/'
+const API_URL = 'https://starfish-app-mzayt.ondigitalocean.app/'
 
 export const api = axios.create({
   baseURL: API_URL
